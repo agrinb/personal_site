caea38297fea86c901eb5d366590d113a7a0f5fe
Alek <agrinb@gmail.com>
2015-03-17 02:48:25 -0400

    Added personal profile

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,20 +1,5 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name alekNgApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the alekNgApp
- */
-// angular.module('alekNgApp')
-//   .controller('MainCtrl', function ($scope) {
-//     $scope.menu = [
-//       'HTML5 Boilerplate',
-//       'AngularJS',
-//       'Karma'
-//     ];
-//   });
 
 angular.module('alekNgApp')
   .controller('MainCtrl', ['$anchorScroll', '$location', '$scope',
