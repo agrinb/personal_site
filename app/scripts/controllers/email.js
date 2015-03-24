47a2bd765037154fd0d7c9c4facc3142722a488c
Alek <agrinb@gmail.com>
2015-03-24 13:54:17 -0400

    moved AJAX call into controller

--- app/scripts/controllers/email.js
+++ app/scripts/controllers/email.js
@@ -7,16 +7,30 @@
  * # MainCtrl
  * Controller of the alekNgApp
  */
-angular.module('alekNgApp')
-  .controller('EmailCtrl', function ($scope, Lightbox) {
-    $scope.images= [ {msg: "Thank you, your email is on it's way. Chat soon!", 
-      url: '/views/lightbox.html'
-    } ];
-    $scope.connectionRequest = {};
-    console.log('I\'m here');
+angular.module('alekNgApp').controller('EmailCtrl', function ($scope, Lightbox) {
+  $scope.images= [ {msg: "Thank you, your email is on it's way. Chat soon!", 
+    url: '/views/lightbox.html'
+  } ];
   $scope.openLightboxModal = function (index) {
     Lightbox.openModal($scope.images, 0);
   };
+
+  $(document).on('submit', "form", function (event) { 
+    event.preventDefault();
+    var message = {
+      name: $('input#name').val(),
+      email:  $('input#email').val(),
+      mbody: $('textarea#message').val()
+    };
+    $.ajax({
+      type: "POST",
+      url: 'http://job-sniper.herokuapp.com/send_email',
+      data: message,
+      success:  function (res) {
+        console.log(res);
+      }
+    });
+  });
 });
 
 angular.module('alekNgApp').config(function (LightboxProvider) {
@@ -24,23 +38,5 @@ angular.module('alekNgApp').config(function (LightboxProvider) {
     return res
   };
   LightboxProvider.templateUrl = '/views/lightbox.html';
-});
-
-$(document).on('submit', "form", function (event) { 
-  event.preventDefault();
-  var message = {
-    name: $('input#name').val(),
-    email:  $('input#email').val(),
-    mbody: $('textarea#message').val()
-  };
-  $.ajax({
-    type: "POST",
-    dataType: 'json',
-    url: 'http://job-sniper.herokuapp.com/send_email',
-    data: message,
-    success:  function (res) {
-      console.log(res);
-    }
-  });
 });
 
